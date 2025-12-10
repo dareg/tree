2e769eec778ee7d43fc399c7b249785cc9f7994b
Judicaël Grasset <judicael.grasset@meteo.fr>
2025-12-10 13:12:51 +0000

    Peux générer des fichiers dot

--- tree2.py
+++ tree2.py
@@ -44,14 +44,15 @@ def remove_contained(proc):
     return proc
 
 
-root = Path(sys.argv[1])
-for filename in root.glob("**/*.F90"):
-    print("Working on ", filename)
+def analyze_file(filename):
+    # print("Working on ", filename)
     file = pyfxtran.run(filename, ["-construct-tag", "-o", "-"])
 
     src = file.replace('xmlns="http://fxtran.net/#syntax"', "")
     # src=simplify_xml(file)
     procs = get_procs(src)
+
+    infos = {}
 
     for proc in procs:
         # proc=remove_contained(proc)
@@ -69,10 +70,24 @@ for filename in root.glob("**/*.F90"):
         if not proc_name:
             continue
         calls = proc.findall(".//call-stmt")
-        callees = []
+        callees = set()
         for call in calls:
-            callees.append(call.find("procedure-designator/named-E/N/n").text)
+            callees.add(call.find("procedure-designator/named-E/N/n").text)
         # print(proc.find(f'./end-subroutine-stmt/subroutine-N/N/n').text)
-        print(f"{proc_name}:")
-        for c in callees:
-            print("\t", c)
+        infos[proc_name] = callees
+    return infos
+
+def generate_dotfile(all_infos):
+    g = "digraph G{\n\tnode [shape=box, style=filled];\n"
+    for info in all_infos:
+        for proc_name in info:
+            g = g + f'{proc_name}[label="{proc_name}"];\n'
+            for callee in info[proc_name]:
+                g = g + f"{proc_name} -> {callee};\n"
+    g = g + "}\n"
+    print(g)
+
+root = Path(sys.argv[1])
+all_infos = []
+for filename in root.glob("**/*.F90"):
+    all_infos.append(analyze_file(filename))
